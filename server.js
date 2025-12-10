1a38d21003eecd2e39bd6d30109b4e60b959d386
unknown <khaledgaber494@gmail.com>
2025-12-10 14:20:08 +0200

    start

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+// Serve static files from the build directory
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
+// Handle all routes by serving index.html
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
